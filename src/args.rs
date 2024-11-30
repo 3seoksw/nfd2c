b68d888d78550c6419deaac2d45676445b9d24d7
WooSeok Kim <3suksw@gmail.com>
2024-12-01 00:28:47 +0900

    !fix: fatal error by removing unnecessary conflict condition

--- src/args.rs
+++ src/args.rs
@@ -11,26 +11,13 @@ use clap::{Parser, ArgGroup};
     ArgGroup::new("input")
         .required(true)
         .args(&["directory"]),
-        // .args(&["files", "directory"]),
 ))]
 pub struct Args {
-    // #[arg(
-    //     short = 'f',
-    //     long = "files",
-    //     value_name = "FILE(S)",
-    //     num_args = 1..,
-    //     default_value = "./",
-    //     conflicts_with = "directory",
-    //     help = "File(s) to process",
-    // )]
-    // pub files: Vec<String>,
-
     #[arg(
         short = 'd',
         long = "directory",
         value_name = "DIR",
         default_value = "./",
-        conflicts_with = "files",
         help = "Directory to process files inside",
     )]
     pub directory: Option<String>,
